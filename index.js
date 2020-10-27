65fb7e696f0cbcbc8661f93a0b3cc8b5002105d1
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-27 16:04:05 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeParagraph();
+});
+
+function changeParagraph() {
+    document.getElementById("text").textContent = "This is really cool!"
+}
